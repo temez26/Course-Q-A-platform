360fb37a387aae3c91ec39cfd1b4b27a1133be31
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2024-04-15 20:34:04 +0300

    Update Course.svelte to include course name and description

--- qa-ui/src/components/Course.svelte
+++ qa-ui/src/components/Course.svelte
@@ -1,22 +1,11 @@
 <script>
-  async function callApi(path, method = "GET", body = {}) {
-    const response = await fetch(path, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-    if (response.ok) {
-      message = await response.text();
-    } else {
-      message = "Error: " + response.status;
-    }
-  }
 </script>
 
 <div>
   <h1>Welcome to the course page</h1>
+  <div class="font-bold text-xl mb-2">Course Name</div>
+
+  <p class="text-gray-700 text-base">Course Description</p>
 </div>
 
 <style>
