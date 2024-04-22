9c861ea10bd06feb7cfb0c716a3022b114734220
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2024-04-22 22:42:00 +0300

    Refactor storage handling in qa-ui/src/stores/stores.js

--- qa-ui/src/stores/stores.js
+++ qa-ui/src/stores/stores.js
@@ -1,6 +1,17 @@
 import { writable } from "svelte/store";
 
-// Variables that use localStorage or subscribe
+function createSyncedStore(key, startValue) {
+  const valueFromStorage = sessionStorage.getItem(key);
+  const initialValue = valueFromStorage
+    ? JSON.parse(valueFromStorage)
+    : startValue;
+  const store = writable(initialValue);
+  store.subscribe((value) =>
+    sessionStorage.setItem(key, JSON.stringify(value))
+  );
+  return store;
+}
+
 let user = localStorage.getItem("userUuid");
 if (!user) {
   user = crypto.randomUUID().toString();
@@ -8,34 +19,12 @@ if (!user) {
 }
 export const userUuid = writable(user);
 
-export let courseId = writable(sessionStorage.getItem("courseId") || 0);
-courseId.subscribe((value) => sessionStorage.setItem("courseId", value));
-
-export const specificQuestionId = writable(
-  sessionStorage.getItem("specificQuestionId") || 0
-);
-specificQuestionId.subscribe((value) =>
-  sessionStorage.setItem("specificQuestionId", value)
-);
-
-export let currentPage = writable(
-  parseInt(sessionStorage.getItem("currentPage")) || 0
-);
-currentPage.subscribe((value) =>
-  sessionStorage.setItem("currentPage", value.toString())
-);
-export let coursepage = writable(
-  parseInt(sessionStorage.getItem("coursepage")) || 0
-);
-coursepage.subscribe((value) =>
-  sessionStorage.setItem("coursepage", value.toString())
-);
-
-export let userAnswer = writable(sessionStorage.getItem("userAnswer") || "");
-userAnswer.subscribe((value) => sessionStorage.setItem("userAnswer", value));
-
-export let question = writable(sessionStorage.getItem("question") || "");
-question.subscribe((value) => sessionStorage.setItem("question", value));
+export let courseId = createSyncedStore("courseId", 0);
+export const specificQuestionId = createSyncedStore("specificQuestionId", 0);
+export let currentPage = createSyncedStore("currentPage", 0);
+export let coursepage = createSyncedStore("coursepage", 0);
+export let userAnswer = createSyncedStore("userAnswer", "");
+export let question = createSyncedStore("question", "");
 
 export let tempId = writable(0);
 export const currentCourse = writable(null);
